c9d98c27bfae846494b441907f729b3686d91499
Attila Szaloki <szalokiattila@gmail.com>
2025-05-03 13:56:15 +0100

    Extend vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.PROD': JSON.stringify(true), // Ensure PROD is injected
+  },
+  
 });
